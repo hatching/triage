5bc04bb2ac6c21f51823a27291a869e6baf769ab
Jurriaan Bremer <jbr@hatching.io>
2022-03-24 09:08:21 +0100

    fix closing parenthesis, thanks @danielpanteleit

--- python/setup.py
+++ python/setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="hatching-triage",
-    version="0.1.6",
+    version="0.1.7",
     author="Hatching B.V.",
     author_email="info@hatching.io",
     description="API client and CLI for Hatching Triage",
