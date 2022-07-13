ca73bf8e16401887e4c79137cfd7f3a3a6818dad
Ricardo van Zutphen <ricardo@hatching.io>
2022-07-13 18:32:54 +0200

    Allow newer requests versions

--- python/setup.py
+++ python/setup.py
@@ -24,7 +24,7 @@ setuptools.setup(
     install_requires=[
         "appdirs==1.4.4",
         "click==8.0.3",
-        "requests==2.25.1"
+        "requests>=2.25.1, <3"
     ],
     python_requires='>=3.6',
 )
