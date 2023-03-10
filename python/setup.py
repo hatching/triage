04a1dbd5b8f75d0678fb861be8e01e973e176179
Ricardo van Zutphen <ricardo@hatching.io>
2023-03-10 18:49:36 +0100

    Bump Python client version to 0.1.9

--- python/setup.py
+++ python/setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="hatching-triage",
-    version="0.1.8",
+    version="0.1.9",
     author="Hatching B.V.",
     author_email="info@hatching.io",
     description="API client and CLI for Hatching Triage",
