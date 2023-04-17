e8a89eefa7e27d6d59fcd8c303ba38eaf5900b96
botlabsDev <54632107+botlabsDev@users.noreply.github.com>
2023-04-17 19:32:01 +0200

    bump click version to 8.1.3

---
+++ python/setup.py
@@ -0,0 +1,30 @@
+# Copyright (C) 2020-2022 Hatching B.V.
+# All rights reserved.
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hatching-triage",
+    version="0.1.9",
+    author="Hatching B.V.",
+    author_email="info@hatching.io",
+    description="API client and CLI for Hatching Triage",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://hatching.io/",
+    packages=setuptools.find_packages(),
+    entry_points={
+        "console_scripts": [
+            "triage = cli.triage:cli",
+        ],
+    },
+    install_requires=[
+        "appdirs==1.4.4",
+        "click==8.1.3",
+        "requests>=2.25.1, <3"
+    ],
+    python_requires='>=3.6',
+)
