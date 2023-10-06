c2298c0f55b0f24b1de2f833600dac02007d2482
Evert Arends <berm@hatching.io>
2023-10-06 11:51:41 +0200

    Fixed: Added filename quote escaping to prevent 400 error from API due to quotes in filename

--- python/triage/client.py
+++ python/triage/client.py
@@ -55,7 +55,7 @@ class Client:
         except exceptions.HTTPError as err:
             raise ServerError(err)
 
-    def submit_sample_file(self, filename, file, interactive=False, profiles=None, password=None, timeout=150, network="internet"):
+    def submit_sample_file(self, filename, file, interactive=False, profiles=None, password=None, timeout=150, network="internet", escape_filename=True):
         """
         Submit a file for analysis on Triage.
 
@@ -80,6 +80,8 @@ class Client:
                 Timeout of the analysis
             network (str):
                 Type of network routing to use ("internet" | "drop" | "tor" | "sim200" | "sim404" | "simnx")
+            escape_filename (bool):
+                Filename escaping to prevent quotation issues
         Returns:
             response (dict):
                 {
@@ -102,6 +104,8 @@ class Client:
                 'network': network
             }
         }
+        if escape_filename:
+            filename = filename.replace('"', '\\"')
         if password:
             d['password'] = password
         body, content_type = encode_multipart_formdata({
